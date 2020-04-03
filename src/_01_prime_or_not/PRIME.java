c01e8d82414fd7eb9c6a6f1d00e45fb377c6631d
root <root@b2fcc0a365c4>
2020-04-03 23:54:34 +0000

    did not do much

---
+++ src/_01_prime_or_not/PRIME.java
@@ -0,0 +1,79 @@
+package _01_prime_or_not;
+
+import javax.swing.JOptionPane;
+
+public class PRIME {
+public static void main(String[] args) {
+	String numba = JOptionPane.showInputDialog("Pick a number?");
+	
+	for (int i = 0; i < 10; i++) {
+		if(numba % 2) {
+			//break();
+			JOptionPane.showMessageDialog(null, "Your numba is not prime.");
+		}
+		if(numba % 3) {
+			//break();
+			JOptionPane.showMessageDialog(null, "Your numba is not prime.");
+		}
+		if(numba % 4) {
+			//break();
+			JOptionPane.showMessageDialog(null, "Your numba is not prime.");
+		}
+		if(numba % 5) {
+			//break();
+			JOptionPane.showMessageDialog(null, "Your numba is not prime.");
+		}
+		if(numba % 6) {
+			//break();
+			JOptionPane.showMessageDialog(null, "Your numba is not prime.");
+		}
+		if(numba % 7) {
+			//break();
+			JOptionPane.showMessageDialog(null, "Your numba is not prime.");
+		}
+		if(numba % 8) {
+			//break();
+			JOptionPane.showMessageDialog(null, "Your numba is not prime.");
+		}
+		if(numba % 9) {
+			//break();
+			JOptionPane.showMessageDialog(null, "Your numba is not prime.");
+		}
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	}
+}
